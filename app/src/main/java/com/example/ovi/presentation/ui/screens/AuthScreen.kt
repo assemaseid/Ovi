a5d2ffcc1d4ad20f80b4fc99d3fe6a0d572d25ff
assemaseid <seidkarim601705@gmail.com>
2026-02-04 04:49:59 +0500

    урааааааа, приложение запускается и открывается страница авторизации, затем, главная страница

--- app/src/main/java/com/example/ovi/presentation/ui/screens/AuthScreen.kt
+++ app/src/main/java/com/example/ovi/presentation/ui/screens/AuthScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.ovi.presentation.viewmodel.AuthViewModel
 import com.example.ovi.ui.theme.OviTheme
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AuthScreen (
     viewModel: AuthViewModel = hiltViewModel(),
