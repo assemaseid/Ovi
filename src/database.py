2b9efb14a7230ab40d1ba9a5dadffbda50319002
Abzal <k4bdoldaev@gmail.com>
2026-02-20 22:47:08 +0500

    feat: implement data models from dfd

--- src/database.py
+++ src/database.py
@@ -1,9 +1,16 @@
-from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import String
+from sqlalchemy import String, Uuid
 from .config import settings
+
+from typing import Annotated
+
+from sqlalchemy import UUID
+from sqlalchemy.orm import mapped_column
+
+import uuid as uuid_lib
+from uuid import uuid4
 
 engine = create_async_engine(
     url=settings.db_url,
@@ -21,13 +28,27 @@ async_session_factory = async_sessionmaker(
     )
 
 str_256 = Annotated[str, 256]
+str_64 = Annotated[str, 64]
+str_20 = Annotated[str, 20]
+str_50 = Annotated[str, 50]
+uuid_pk = Annotated[
+    uuid_lib.UUID,
+    mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+]
+uuid_fk = Annotated[
+    uuid_lib.UUID,
+    mapped_column(UUID(as_uuid=True)),
+]
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
     
     type_annotation_map = {
-        str_256: String(256)
+        str_256: String(256),
+        str_64: String(64),
+        str_20: String(20),
+        str_50: String(50),
     }
     
     
