de90500ffb4513210f2f3fd6f06343c0020471b5
assemaseid <seidkarim601705@gmail.com>
2026-02-24 18:05:28 +0500

    Updated ui of Bluetooth Screen

--- android/app/src/main/java/com/example/ovi/presentation/ui/screens/BluetoothScreen.kt
+++ android/app/src/main/java/com/example/ovi/presentation/ui/screens/BluetoothScreen.kt
@@ -1,18 +1,47 @@
 package com.example.ovi.presentation.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ovi.ui.theme.*
 
 @Composable
-fun BluetoothScreen() {
+fun BluetoothScreen(
+    onBack: () -> Unit = {}
+) {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(BgTop, BgBottom)
+                )
+            )
     ) {
-        Text(text = "Connect to lock")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+        ) {
+            Spacer(modifier = Modifier.height(48.dp))
+
+
+                Text(
+                    text = "Pair Lock",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = TextWhite
+                )
+
+        }
     }
 }
