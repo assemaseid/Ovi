dd8bfd6b9088cabe48c2a6f0fdadeabf88ed94f5
assemaseid <seidkarim601705@gmail.com>
2026-02-24 18:06:04 +0500

    Locks Screen's ui updated

--- android/app/src/main/java/com/example/ovi/presentation/viewmodel/DevicesViewModel.kt
+++ android/app/src/main/java/com/example/ovi/presentation/viewmodel/DevicesViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.ovi.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.ovi.domain.model.DeviceItem
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -10,21 +11,13 @@ class DevicesViewModel: ViewModel(){
 
     private val _devices = MutableStateFlow(
         listOf(
-            DeviceItem("lock-1", "Smart Lock #1", "ON", true),
-            DeviceItem("lock-2", "Smart Lock #2", "OFF", true),
-            DeviceItem("lock-3", "Garage Lock", "ON", false)
+            DeviceItem("lock-1", "Smart Lock #1", 64, false, "20:30"),
+            DeviceItem("lock-2", "Smart Lock #2", 25, true, "18:45"),
+            DeviceItem("lock-3", "Garage Lock", 100, false, "12:00")
         )
     )
 
     val devices: StateFlow<List<DeviceItem>> = _devices
-
-    fun toggleLock(deviceId: String){
-        _devices.value = _devices.value.map { device ->
-            if (device.id == deviceId) {
-                device.copy(locked = !device.locked)
-            } else device
-        }
-    }
 
     fun getDevice(deviceId: String): DeviceItem? {
         return _devices.value.find { it.id == deviceId }
