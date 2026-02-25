1764baa00e62e7ebadbca986291c41f58fb60146
Abzal <k4bdoldaev@gmail.com>
2026-02-25 23:18:30 +0500

    feat(mqtt): implement singleton MQTTService

--- src/main.py
+++ src/main.py
@@ -5,13 +5,16 @@ from src.app.api.v1 import auth
 from starlette.middleware.cors import CORSMiddleware
 
 from src.app.queries.orm import AsyncOrm
+from src.app.services.device_service import mqtt_service
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await AsyncOrm.create_tables()
     await AsyncOrm.insert_users()
-
+    await mqtt_service.connect()
+    await mqtt_service.subscribe("devices/#")
     yield
+    await mqtt_service.disconnect()
 
 def create_app() -> FastAPI:
     api_v1 = APIRouter(prefix="/api/v1")
