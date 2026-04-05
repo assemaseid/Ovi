bcdcd718a981db22b988d9936b8d7e8a9059eddf
Abzal <k4bdoldaev@gmail.com>
2026-04-05 16:07:18 +0500

    refactor auth_service

---
+++ src/app/services/auth_service.py
@@ -0,0 +1,86 @@
+import uuid
+from datetime import timedelta, datetime, UTC
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.app.models.auth import TokenBlacklist
+from src.app.models.user import User
+from src.app.schemas.auth import TokenResponse
+from src.app.schemas.user import UserResponseSchema
+from src.config  import settings
+from src.app.security import jwt_utils
+
+TOKEN_TYPE_FIELD = "type"
+_ACCESS_TOKEN_TYPE = "access"
+_REFRESH_TOKEN_TYPE = "refresh"
+
+def create_token(
+    token_type: str,
+    token_data: dict, 
+    expire_minutes: int,
+    expire_timedelta: timedelta | None = None , 
+    ) -> str:
+    jwt_payload = {TOKEN_TYPE_FIELD: token_type}
+    jwt_payload.update(token_data)
+    return jwt_utils.encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+        )
+
+def create_access_token(user: UserResponseSchema) -> str:
+    jti = str(uuid.uuid4())
+    jwt_payload = {
+        "sub": str(user.user_uuid),
+        TOKEN_TYPE_FIELD: _ACCESS_TOKEN_TYPE,
+        "email": user.email,
+        "jti": jti,
+        "iat": datetime.now(UTC),
+    }
+    
+    return create_token(
+        token_type=_ACCESS_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_minutes=settings.auth_jwt.access_token_expire_minutes,
+        )
+
+def create_refresh_token(user: UserResponseSchema) -> str:
+    jti = str(uuid.uuid4())
+    jwt_payload = {
+        "sub": str(user.user_uuid),
+        TOKEN_TYPE_FIELD: _ACCESS_TOKEN_TYPE,
+        "email": user.email,
+        "jti": jti,
+        "iat": datetime.now(UTC),
+    } 
+    return create_token(
+        token_type=_REFRESH_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_timedelta=timedelta(days=settings.auth_jwt.refresh_token_expire_days),
+        )
+
+def create_token_pair(user: UserResponseSchema):
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
+    return TokenResponse(
+        access_token=access_token,
+        refresh_token=refresh_token,
+        user_data=user.model_dump(),
+    )
+
+async def get_user_by_email(db: AsyncSession, email: str | None) -> User | None:
+    result = await db.execute(select(User).where(
+        User.email==uuid.UUID(str(email))))
+    return result.scalar_one_or_none()
+
+async def get_user_by_uuid(db: AsyncSession, user_uuid: str | None) -> User | None:
+    result = await db.execute(select(User).where(
+        User.user_uuid==uuid.UUID(str(user_uuid))))
+    return result.scalar_one_or_none()
+
+
+async def is_token_revoked(db: AsyncSession, jti: str) -> bool:
+    result = await db.execute(select(TokenBlacklist).where(
+        TokenBlacklist.token_id==jti))
+    return bool(result.scalar_one_or_none())
